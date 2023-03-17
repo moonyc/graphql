9927818d7a5a1224eded768f69986568c8b8cb55
muni <booscarface@gmail.com>
2023-03-17 18:09:52 +0100

    comments and relations

--- 1-basics/src/index.js
+++ 1-basics/src/index.js
@@ -20,22 +20,45 @@ const users = [{
     name: 'Marjorie',
     email: 'marjorie@marjoriethesnake.com'
 }]
+
 // Demo Post Data
 const posts = [{
-   id: '1',
+   id: '4',
    title: '1',
    body: '1',
-   published: true
+   published: true,
+   author: '1'
 },{
-    id: '2',
+    id: '5',
     title: '2',
     body: '2',
-    published: true
+    published: true,
+    author: '2'
  },{
-    id: '3',
+    id: '6',
     title: '3',
     body: '3',
-    published: false
+    published: false,
+    author: '3'
+ }]
+
+ // Demo Comment Data
+ const comments = [{
+    id: '7',
+    text: 'Nice!',
+    author: '3',
+    post: '5'
+ },
+ {
+    id: '8',
+    text: 'Brilliant!',
+    author: '2',
+    post: '5'
+ },{
+    id: '9',
+    text: 'Sasha did it again!',
+    author: '1',
+    post: '6'
  }]
 
 const schema = createSchema({
@@ -43,6 +66,7 @@ const schema = createSchema({
    type Query {
      posts(query: String): [Post!]!
      users(query: String): [User!]!
+     comments(query: String): [Comment!]!
      me: User!
      firstPost: Post!
     }
@@ -52,12 +76,22 @@ const schema = createSchema({
         name: String!
         email: String!
         age: Int
+        posts: [Post!]!
+        comments: [Comment!]!
     }
     type Post {
         id: ID!
         title: String!
         body: String!
         published: Boolean!
+        author: User!
+        comments: [Comment!]!
+    }
+    type Comment {
+        id: ID!
+        text: String!
+        author: User!
+        post: Post!
     }
 `,
 resolvers: {
@@ -80,6 +114,14 @@ resolvers: {
             return isTitleMatch || isBodyMatch
          })
       },
+      comments(parent, args, ctx, info) {
+         if(!args.query) {
+            return comments
+         }
+         return comments.filter((comment) => {
+            return comment.text.toLowerCase().includes(args.query.toLowerCase())
+         })
+      },
       me() {
         return {
             id: 'gattini12345',
@@ -96,6 +138,44 @@ resolvers: {
         }
 
       }
+    },
+    Post: {
+        author(parent, args, ctx, info) {
+          return users.find((user) => {
+              return user.id === parent.author
+          })
+        },
+        comments(parent, args, ctx, info) {
+           return comments.filter((comment) => {
+            return comment.post === parent.id
+           })
+        }
+    },
+    User: {
+        posts(parent, args, ctx, info) {
+            return posts.filter((post) => {
+                return post.author === parent.id
+            })
+        },
+        comments(parent, args, ctx, info) {
+          return comments.filter((comment) => {
+            return comment.author === parent.id
+          })
+        }
+    },
+    Comment : {
+        post(parent, args, ctx, info) {
+          return posts.find((post) => {
+             return post.id === parent.post
+          })
+        },
+        author(parent, args, ctx, info) {
+           return users.find((user) => {
+            return user.id === parent.author
+           })
+        }
+        
+
     }
  }
 })
