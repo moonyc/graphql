c10733ca2cfd481d60e2680cea343530fff0dc71
muni <booscarface@gmail.com>
2023-03-17 16:25:05 +0100

    array and reduce method on arrays

--- 1-basics/src/index.js
+++ 1-basics/src/index.js
@@ -6,13 +6,15 @@
  * Scalar Types: String, Boolean, Int, Float, ID (unique to graphql)
  * A scalar type is a type that stores a single value 
  */
+
 const schema = createSchema({
     typeDefs: /*Type definition */`
    type Query {
      greeting(name: String, position: String): String!
+     grades: [Int!]!
      me: User!
      firstPost: Post!
-     add(a: Float!, b: Float!): Float!
+     add(numbers: [Float!]!): Float!
     }
     
     type User {
@@ -37,10 +39,16 @@ resolvers: {
         return 'Hello' 
       },
       add(parent, args, ctx, info) {
-        
-            return args.a + args.b
-       
-        
+        if (args.numbers.length === 0) {
+            return 0
+        }
+
+        return args.numbers.reduce((accumulator, currentValue) => {
+           return accumulator + currentValue
+        })
+      },
+      grades(parent, args, ctx, info) {
+        return [99, 80, 93]
       },
       me() {
         return {
